f689fb69003e6682fe693c5cc50a6c5fce9b6e8a
Stephen Gontzes <gontzess@gmail.com>
2022-03-15 18:37:47 -0400

    Initial demo configuration with service1, service2, fluent-bit config, gateway collector config
    Co-authored-by: Isaak Krautwurst <isaakkraut@gmail.com>
    Co-authored-by: Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
    Co-authored-by: Josh Keller <jrkeller@gmail.com>

---
+++ services/service2/basic-tracing.js
@@ -0,0 +1,43 @@
+const opentelemetry = require("@opentelemetry/sdk-node");
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-grpc");
+
+const { Resource } = require("@opentelemetry/resources");
+
+const {
+  SemanticResourceAttributes,
+} = require("@opentelemetry/semantic-conventions");
+
+const exporter = new OTLPTraceExporter({
+  // optional - url default value is http://localhost:55681/v1/traces
+  url: "http://localhost:4317/v1/traces",
+});
+
+const sdk = new opentelemetry.NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "service2",
+  }),
+  traceExporter: exporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+// initialize the SDK and register with the OpenTelemetry API
+// this enables the API to record telemetry
+sdk
+  .start()
+  .then(() => console.log("Tracing initialized"))
+  .catch((error) => console.log("Error initializing tracing", error));
+
+// gracefully shut down the SDK on process exit
+process.on("SIGTERM", () => {
+  sdk
+    .shutdown()
+    .then(() => console.log("Tracing terminated"))
+    .catch((error) => console.log("Error terminating tracing", error))
+    .finally(() => process.exit(0));
+});
